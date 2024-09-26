93e4f668f88256bc65d8351991c16cf29f0c33e7
ryoji-ishii <goccha.dev@gmail.com>
2024-09-26 21:26:51 +0900

    add EvalFunc

--- ternaries/ternaries.go
+++ ternaries/ternaries.go
@@ -1,8 +1,17 @@
 package ternaries
 
+type F[T any] func() T
+
 func Eval[T any](is bool, trueValue, falseValue T) T {
 	if is {
 		return trueValue
 	}
 	return falseValue
+}
+
+func EvalFunc[T any](is bool, trueFunc F[T], falseFunc F[T]) T {
+	if is {
+		return trueFunc()
+	}
+	return falseFunc()
 }
